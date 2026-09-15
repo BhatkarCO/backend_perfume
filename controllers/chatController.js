5cf96285762a3d12c402359b8fa3bc7a554d4fcf
rohanN8740 <rohannandavdekar8983@gmail.com>
2026-09-15 12:28:32 +0530

    fixed chat services

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const chatWithAI = async (req, res) => {
   try {
     const response = await axios.post(
-      "https://ai-service-perfume.onrender.com",
+      "https://ai-service-perfume.onrender.com/chat",
       req.body,
     );
 
